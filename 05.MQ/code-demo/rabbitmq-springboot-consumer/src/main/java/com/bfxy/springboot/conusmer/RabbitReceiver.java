320b37a9c3c66078e653721f03d4329080320d02
gcc135 <1442458224@qq.com>
2020-01-07 08:22:01 +0800

    添加rabbitmq和kafka区别的梳理

--- 05.MQ/code-demo/rabbitmq-springboot-consumer/src/main/java/com/bfxy/springboot/conusmer/RabbitReceiver.java
+++ 05.MQ/code-demo/rabbitmq-springboot-consumer/src/main/java/com/bfxy/springboot/conusmer/RabbitReceiver.java
@@ -40,7 +40,6 @@ public class RabbitReceiver {
 	
 	
 	/**
-	 * 
 	 * 	spring.rabbitmq.listener.order.queue.name=queue-2
 		spring.rabbitmq.listener.order.queue.durable=true
 		spring.rabbitmq.listener.order.exchange.name=exchange-1
